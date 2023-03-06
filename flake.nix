bb454ca222a88b41eb4474ee65784a260d819b9a
Jörg Thalheim <joerg@thalheim.io>
2023-03-06 11:31:33 +0100

    add project boiler code

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Development environment for this project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
+      systems = lib.systems.flakeExposed;
+      perSystem = { pkgs, ... }: {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          name = "buildxzy";
+          src = self;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+      };
+    });
+}
