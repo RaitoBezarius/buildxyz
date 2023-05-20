9d25d4afe57ecf1deec575e73f6489fae0c120fe
Raito Bezarius <masterancpp@gmail.com>
2023-05-21 01:04:10 +0200

    contrib: implement sandboxing via bubblewrap

--- contrib/iterate_over_pypi_dump.sh
+++ contrib/iterate_over_pypi_dump.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --pure -i bash -p jq git nix --keep BUILDXYZ_NIXPKGS --keep RUST_BACKTRACE
+#!nix-shell --pure -i bash -p jq git nix bubblewrap which --keep BUILDXYZ_NIXPKGS --keep RUST_BACKTRACE
 # shellcheck shell=sh
 
 pypi_buildxyz() {
   package="$1"
   echo "buildxyz $package"
-  ./target/debug/buildxyz --automatic --record-to "examples/python/$package.toml" "pip install $package --prefix /tmp --no-binary :all:"
+  bwrap \
+  --bind /nix /nix \
+  --dev-bind /dev /dev \ # Necessary for /dev/fuse…
+  --ro-bind $(which git) $(which git) \
+  --ro-bind $(pwd)/target $(pwd)/target \
+  --bind $(pwd)/examples $(pwd)/examples \
+  --tmpfs /buildxyz \
+  --proc /proc \
+  --unshare-pid \
+  --cap-add CAP_SYS_ADMIN \ # Necessary for setuid
+  --new-session \
+    ./target/debug/buildxyz --automatic --record-to "examples/python/$package.toml" "pip install $package --prefix /tmp --no-binary :all:"
 }
 
 jq -rc '.rows | .[] | .project' top-pypi.json | while read -r package; do
--- contrib/pack_from_pypi.sh
+++ contrib/pack_from_pypi.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --pure -i bash -p jq git nix rustc --keep BUILDXYZ_NIXPKGS --keep RUST_BACKTRACE
+#!nix-shell --pure -i bash -p jq git nix bubblewrap which --keep BUILDXYZ_NIXPKGS --keep RUST_BACKTRACE
 # shellcheck shell=sh
 
 pypi_buildxyz() {
   package="$1"
   echo "buildxyz $package"
-  ./target/debug/buildxyz --record-to "examples/python/$package.toml" "pip install $package --prefix /tmp --no-binary :all:"
+  export TMPDIR="/buildxyz"
+  bwrap \
+  --bind /nix /nix \
+  --dev-bind /dev /dev \
+  --ro-bind $(which git) $(which git) \
+  --ro-bind $(pwd)/target $(pwd)/target \
+  --bind $(pwd)/examples $(pwd)/examples \
+  --tmpfs /buildxyz \
+  --proc /proc \
+  --unshare-pid \
+  --cap-add CAP_SYS_ADMIN \
+  --new-session \
+    ./target/debug/buildxyz --record-to "examples/python/$package.toml" "pip install $package --prefix /tmp --no-binary :all:"
 }
 
 pypi_buildxyz "$1"
