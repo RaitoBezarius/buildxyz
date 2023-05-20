b07d8599b5d6f6f95ea8afec6eca9e067043256e
Raito Bezarius <masterancpp@gmail.com>
2023-05-20 23:50:01 +0200

    contrib: fix our dumping scripts

--- contrib/iterate_over_pypi_dump.sh
+++ contrib/iterate_over_pypi_dump.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --pure -i bash -p jq git nix --keep BUILDXYZ_NIXPKGS
+#!nix-shell --pure -i bash -p jq git nix --keep BUILDXYZ_NIXPKGS --keep RUST_BACKTRACE
 # shellcheck shell=sh
 
 pypi_buildxyz() {
   package="$1"
   echo "buildxyz $package"
-  ../target/debug/buildxyz --automatic --record-to "examples/python/$package.toml" "pip install $package --prefix /tmp --no-binary :all:"
+  ./target/debug/buildxyz --automatic --record-to "examples/python/$package.toml" "pip install $package --prefix /tmp --no-binary :all:"
 }
 
 jq -rc '.rows | .[] | .project' top-pypi.json | while read -r package; do
