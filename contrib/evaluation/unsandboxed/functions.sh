65dced8824fd663990f904af99c2a5d1760dd2c4
Raito Bezarius <masterancpp@gmail.com>
2023-07-21 14:09:35 +0200

    evaluation(parallel): fix the variables exportation to the parallel workers

--- contrib/evaluation/unsandboxed/functions.sh
+++ contrib/evaluation/unsandboxed/functions.sh
@@ -1,11 +1,11 @@
-BUILDXYZ_RELEASE_VARIANT="release"
-BUILDXYZ_BINARY="./target/$BUILDXYZ_RELEASE_VARIANT/buildxyz"
+export BUILDXYZ_RELEASE_VARIANT="release"
+export BUILDXYZ_BINARY="./target/$BUILDXYZ_RELEASE_VARIANT/buildxyz"
 
-buildxyz_global_flags=()
+export buildxyz_global_flags=()
 
 # Debugging infrastructure
-STRACE=""
-STRACE_EXTRA_FLAGS=""
+export STRACE=""
+export STRACE_EXTRA_FLAGS=""
 
 if [[ -v ENABLE_STRACE ]]; then
   STRACE="strace -yy -f $STRACE_EXTRA_FLAGS"
@@ -20,5 +20,6 @@ pypi_buildxyz() {
   package="$1"
   PREFIX_DIR=$(mktemp -d)
   echo "buildxyz $package in pip prefix $PREFIX_DIR"
+  echo $STRACE $BUILDXYZ_BINARY --record-to "examples/python/$package.toml" "pip install --use-feature=no-binary-enable-wheel-cache --prefix $PREFIX_DIR --no-binary :all: --no-cache-dir $package"
   $STRACE $BUILDXYZ_BINARY "${buildxyz_global_flags[@]}" --record-to "examples/python/$package.toml" "pip install --use-feature=no-binary-enable-wheel-cache --prefix $PREFIX_DIR --no-binary :all: --no-cache-dir $package"
 }
